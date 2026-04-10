ddaf2e2ce1e9f30385f84ed58505862d12fab3f2
Abdellah-Moukarrame <moukarrameabdellah4@gmail.com>
2026-04-10 23:50:08 +0200

    add the routing for the admin

--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,20 @@ Route::controller(GoogleController::class)->group(function () {
     Route::get('/auth/callback/{provider}', 'callback')->name('google.callback');
     Route::get('/auth/redirect/{provider}', 'redirect')->name('google.redirect');
 });
+
+// routes/web.php
+
+Route::prefix('admin')->name('admin.')->group(function () {
+
+    // Dashboard
+    Route::get('/dashboard', fn() => view('admin.dashboard'))->name('dashboard');
+
+    // Teachers
+    Route::get('/teachers', fn() => view('admin.teachers.index'))->name('teachers.index');
+    Route::get('/teachers/{teacher}', fn() => view('admin.teachers.show'))->name('teachers.show');
+
+    // Users
+    Route::get('/users', fn() => view('admin.users.index'))->name('users.index');
+    Route::get('/users/{user}', fn() => view('admin.users.show'))->name('users.show');
+
+});
