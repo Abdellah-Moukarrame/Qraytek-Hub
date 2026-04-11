65bfd718df43e63620bdc539c3a2d736d499bb00
Abdellah-Moukarrame <moukarrameabdellah4@gmail.com>
2026-04-11 22:54:34 +0200

    modify the route name of registring a teacher

--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [HomeController::class, 'index']);
 Route::get('/login', [LoginController::class, 'index'])->name('login');
 Route::get('/register', [RegisterController::class, 'index'])->name('register');
-Route::get('/register_teacher', [RegisterController::class, 'index'])->name('register_teacher');
+Route::get('/register/teacher', [RegisterController::class, 'index'])->name('register.teacher');
 /// Socialite Auth
 Route::controller(GoogleController::class)->group(function () {
     Route::get('/auth/callback/{provider}', 'callback')->name('google.callback');
