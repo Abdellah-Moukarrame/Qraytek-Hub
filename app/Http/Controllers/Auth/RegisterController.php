50ada2d30175e291f2fa1f1409c9e7fe13ef716e
Abdellah-Moukarrame <moukarrameabdellah4@gmail.com>
2026-04-14 18:21:43 +0200

    add the register part

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -3,14 +3,25 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\Personnes;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class RegisterController extends Controller
 {
     public function index(){
-        return view('auth.register');
+        return view("auth.register");
     }
-    public function teachers(){
-        return view('auth.register-teacher');
+    public function store(Request $request){
+
+
+        Personnes::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+            'role'=>'student',
+        ]);
+
+        return redirect()->route("login.index");
     }
 }
