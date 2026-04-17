1eeb448b0502ddd39e84bbbc5746d6c5b962dfcd
Abdellah-Moukarrame <moukarrameabdellah4@gmail.com>
2026-04-17 23:46:42 +0200

    modify on the Personnes Model

--- app/Models/Personnes.php
+++ app/Models/Personnes.php
@@ -4,10 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
 
 
 class Personnes extends Authenticatable
+
 {
+    use Notifiable;
 
     protected $fillable = ['id','name','role','email','password'];
 
