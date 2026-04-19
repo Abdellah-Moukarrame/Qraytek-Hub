703159a86e411efc73183f4a8ae0f12f413593b0
Abdellah-Moukarrame <moukarrameabdellah4@gmail.com>
2026-04-19 19:51:56 +0200

    fix some issues on failiable

--- app/Models/Personne/Teacher.php
+++ app/Models/Personne/Teacher.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Teacher extends Model
 {
-    //
+    protected $fillable = [
+        'personne_id',
+        'subject',
+        'experience',
+        'status',
+        'hourly_rate',
+        'cv_path',
+        'certificate_path',
+        'diploma_path',
+        'id_card_path',
+    ];
 }
