9a3eab0633a232ee5261336be34173ab95ab1450
Abdellah-Moukarrame <moukarrameabdellah4@gmail.com>
2026-04-27 23:52:55 +0200

    update the dash controller

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -3,15 +3,17 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Http\Controllers\StatsController;
 use App\Models\Personnes;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
-    public function index()
+    public function index(StatsController $stats)
     {
+        $data = $stats->adminStats();
         $users = Personnes::where('role','!=','admin')->paginate(15);
-        return view('admin.dashboard', compact('users'));
+        return view('admin.dashboard', compact('users','data'));
     }
 
     public function display_users() {}
